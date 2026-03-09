53821cd6538e6641d91e446a4d28035b9e9787f7
yugcodes-bit <yugantvbschool08@gmail.com>
2026-03-10 05:11:21 +0530

    hangman

--- hangman_day1.py
+++ hangman_day1.py
@@ -5,7 +5,44 @@ chosen_word = random.choice(word_list)
 
 print(chosen_word)
 
+placeholder =""
+for i in range(len(chosen_word)):
+    placeholder += "_"
+print (placeholder)
 
-guess = input("Guess a letter: ").lower()
+game_over = False
+correct_letters = []
+lives = 6
+while not game_over:
+    guess = input("Guess a letter: ").lower()
 
+    if guess in correct_letters:
+        print("Already guessed the letter")
+    display =""
+    
+    for a in chosen_word:
+        if(a == guess):
+            display += a
+            correct_letters.append(guess)    
+        elif a in correct_letters:
+            display += a     #if 
+        else:
+            display += "_"
+
+    
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You guessed {guess} ,its not in the word")
+        if lives == 0:
+            game_over = True
+            print("You lose")        
+
+    if "_" not in display:
+        game_over=True
+        print("You win")
+    print(display)     
+    print(lives)
+
+
+   
 
