4aff6c52c7559c8c7f4974bb98721d4a4e620287
yugcodes-bit <yugantvbschool08@gmail.com>
2026-03-09 22:51:52 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,29 @@
+
+
+# #1. in built functions
+# print("Hello, World!")  # in built function
+    
+# a = len("Hello") # len function to calculate the length of string(in built)
+# print(a)
+
+
+
+
+
+
+# # making your own functions, def (defining func)
+
+# def my_func():        
+#     print("Hello")
+#     print("Bye")
+
+# my_func()   # calling the function
+
+
+
+
+
+
+
+
+
---
+++ hangman_day1.py
@@ -0,0 +1,11 @@
+import random
+
+word_list = ['baboona', 'camel', 'giraffe']
+chosen_word = random.choice(word_list)
+
+print(chosen_word)
+
+
+guess = input("Guess a letter: ").lower()
+
+
